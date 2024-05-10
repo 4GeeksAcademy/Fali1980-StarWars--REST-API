feff96f684d9ba6a468773b21b808936bd3f8449
Rafael <154633798+Fali1980@users.noreply.github.com>
2024-05-10 16:22:54 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -31,6 +31,9 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
+# INICIO DE CÓDIGO-------------------------------
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
@@ -38,12 +41,77 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    all_users = User.query.all()
+    print(all_users)
+    results = list(map(lambda user: user.serialize(), all_users))
+    print(results)
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "LEER LOS USUARIOS "
+    }
+
+    return jsonify(results), 200
+
+@app.route('/character', methods=['GET'])
+def get_characters():
+    all_characters = Character.query.all()
+    print(all_characters)
+    results = list(map(lambda character: character.serialize(), all_characters))
+    print(results)
+
+    response_body = {
+        "msg": "LEER LOS CHARACTERS "
+    }
+
+    return jsonify(results), 200
+
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_character(character_id):
+    character = Character.query.filter_by(id=character_id).first()
+    return jsonify(character.serialize()), 200
+
+@app.route('/planet', methods=['GET'])
+def get_planets():
+    all_planets = Planet.query.all()
+    print(all_planets)
+    results = list(map(lambda planet: planet.serialize(), all_planets))
+    print(results)
+
+    response_body = {
+        "msg": "LEER LOS PLANETS "
+    }
+
+    return jsonify(results), 200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.filter_by(id=planet_id).first()
+    return jsonify(planet.serialize()), 200
+
+
+@app.route('/character', methods=['POST'])
+def create_character():
+    # Leer los datos que envia solicitud (body)
+    data = request.json
+    print(data)
+    if not "name" in data:
+        return jsonify("debes enviar el character"), 400
+    if data["name"] == "":
+        return jsonify("el character no debe ser vacio"), 400
+    print(data.get("name"))
+    print(data["name"])
+    # Crear character nuevo
+    charact = Character(**data)
+    db.session.add(charact)
+    db.session.commit()
+
+    response_body = {
+        "msg": "CREAR LOS USUARIOS "
     }
 
     return jsonify(response_body), 200
+
+# FIN DE CÓDIGO-----------------------------------
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,11 +9,48 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "esta_vivo": self.is_active,
+            # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable = False)
+    height = db.Column(db.String(250), nullable = False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "height": self.height,
+            # do not serialize the password, its a security breach
+        }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    diameter = db.Column(db.String(250), nullable = False)
+    rotation_period = db.Column(db.String(250), nullable = False)
+    climate = db.Column(db.String(250), nullable = False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "climate": self.climate,
             # do not serialize the password, its a security breach
         }
